4a157f0918fd180e8befb1370d752266dc878121
rjiiagl <rjiiagl@gmail.com>
2020-12-22 19:09:20 +0800

    加入專案檔案。

---
+++ FastPixel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace 字模製作與目標比對
+{
+    class FastPixel
+    {
+        public int imagewidth, imageheight;//影像寬與高
+        public byte[,] arrayR, arrayG, arrayB;//RGB陣列
+        byte[] arrayRGB; //影像的可存取副本資料陣列
+        System.Drawing.Imaging.BitmapData imageData;//影像資料
+        IntPtr imageptr; //影像資料所在的記憶體指標(位置)
+        int imageTotalBit, singleRowBit, singlePointBit;//影像總位元組數,單列位元組數，單點位元組數
+
+        //鎖定點陣圖(Bitmap)物件的記憶體位置，建立一個可操作的位元組陣列副本
+        private void LockBMP(Bitmap bmp)
+        {
+            //矩形物件，定義影像範圍
+            Rectangle rectangle = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            //鎖定影像區記憶體(暫時不接受作業系統的移動)
+            imageData = bmp.LockBits(rectangle,System.Drawing.Imaging.ImageLockMode.ReadWrite,bmp.PixelFormat);
+            imageptr = imageData.Scan0;//影像區塊的記憶體指標
+            singleRowBit = imageData.Stride;//每一影像列的長度(bytes)
+            singlePointBit = (int)Math.Floor((double)singleRowBit / (double)bmp.Width);//每一像素的位元組數(3或4)
+            imageTotalBit = singleRowBit * bmp.Height;//影像總位元組數
+            arrayRGB = new byte[imageTotalBit];//宣告影像副本資料陣列
+            //拷貝點陣圖資料到副本陣列
+            System.Runtime.InteropServices.Marshal.Copy(imageptr, arrayRGB, 0, imageTotalBit);
+        }
+
+        //複製位元組陣列副本的處理結果到BitMap物件，並解除其記憶體鎖定
+        private void UnLockBMP(Bitmap bmp)
+        {
+            //拷貝副本陣列到點陣圖位置
+            System.Runtime.InteropServices.Marshal.Copy(arrayRGB, 0, imageptr, imageTotalBit);
+            bmp.UnlockBits(imageData);//解除鎖定
+        }
+
+        //取得RGB陣列
+        public void BMP2RGB(Bitmap bmp)
+        {
+            imagewidth = bmp.Width;
+            imageheight = bmp.Height;//影像寬高
+            arrayR = new byte[imagewidth, imageheight];
+            arrayG = new byte[imagewidth, imageheight];
+            arrayB = new byte[imagewidth, imageheight];//RGB
+            LockBMP(bmp);
+            for(int j = 0;j<imageheight;j++)
+            {
+                int Lj = j * imageData.Stride;
+                for(int i=0;i<imagewidth;i++)
+                {
+                    int k = Lj + i * singlePointBit;
+                    arrayR[i, j] = arrayRGB[k + 2];//Red
+                    arrayG[i, j] = arrayRGB[k + 1];//Green
+                    arrayB[i, j] = arrayRGB[k];//Blue
+                }
+            }
+            UnLockBMP(bmp);
+        }
+
+        //灰階圖
+        public Bitmap GrayImg(byte [,] b)
+        {
+            Bitmap bmp = new Bitmap(b.GetLength(0), b.GetLength(1));
+            LockBMP(bmp);
+            for(int j= 0;j<b.GetLength(1);j++)
+            {
+                for(int i=0;i<b.GetLength(0);i++)
+                {
+                    int k = j * singleRowBit + i * singlePointBit;
+                    byte c = b[i, j];
+                    arrayRGB[k] = c;
+                    arrayRGB[k + 1] = c;
+                    arrayRGB[k + 2] = c;//RGB一致
+                    arrayRGB[k + 3] = 255;//實心不透明
+                }
+            }
+            UnLockBMP(bmp);
+            return bmp;
+        }
+
+        //黑白圖
+        public Bitmap BWImg(byte[,] b)
+        {
+            Bitmap bmp = new Bitmap(b.GetLength(0), b.GetLength(1));
+            LockBMP(bmp);
+            for(int j = 0; j < b.GetLength(1); j++)
+            {
+                for(int i= 0; i < b.GetLength(0); i++)
+                {
+                    int k = j * singleRowBit + i * singlePointBit;
+                    if (b[i, j] == 1)
+                    {
+                        arrayRGB[k] = 0;
+                        arrayRGB[k + 1] = 0;
+                        arrayRGB[k + 2] = 0;//黑
+                    }
+                    else
+                    {
+                        arrayRGB[k] = 255;
+                        arrayRGB[k + 1] = 255;
+                        arrayRGB[k + 2] = 255;//白
+                    }
+                    arrayRGB[k + 3] = 255;
+                }
+            }
+            UnLockBMP(bmp);
+            return bmp;
+        }
+    }
+
+    class TgInfo
+    {
+        public int targetPoint = 0;//目標點數
+        public ArrayList targetPointList = null;//目標點的集合
+        public int x_max_negative = 0, x_max_positive = 0, y_max_negative = 0, y_max_positive = 0;//四面座標極值
+        public int width = 0, height = 0;//寬與高
+        public int contrast_target_back = 0;//目標與背景的對比強度
+        public int x_target = 0, y_target = 0;//目標中心點座標
+        public int ID_contrast = 0;//依對比度排序的序號
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 字模製作與目標比對
+{
+    public partial class Form1 : Form
+    {
+        static int width_word = 25;
+        static int height_word = 50;//字模的寬與高
+        byte[,,,] array_License_Plate = new byte[2, 36, width_word, height_word];//六與七碼車牌所有英數字二值化陣列
+        byte[,,] array_Deform_69 = new byte[2, width_word, height_word];//變形的六碼車牌6與9字型
+        byte[,] array_Compared_Font = new byte[width_word, height_word];
+        byte[,] array_Targect = new byte[width_word, height_word];//待比對的字模與目標二值化陣列
+        FastPixel f = new FastPixel();//宣告快速影像處理物件
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //用影像載入字模
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string d = folderBrowserDialog1.SelectedPath;
+                for(int k = 0; k < 36; k++)
+                {
+                    //六碼字型
+                    string kk = k.ToString();
+                    if (kk.Length == 1) kk = "0" + kk;
+                    Bitmap b6 = new Bitmap(d + "\\" + kk + ".gif");
+                    for(int j = 0; j < height_word; j++)
+                    {
+                        for(int i = 0; i < width_word; i++)
+                        {
+                            Color C = b6.GetPixel(i, j);
+                            if (C.R < 128) array_License_Plate[0, k, i, j]=1;//黑點
+                        }
+                    }
+                    //七碼字型
+                    kk = (k + 36).ToString();
+                    Bitmap b7 = new Bitmap(d + "\\" + kk + ".gif");
+                    for(int j = 0; j < height_word; j++)
+                    {
+                        for(int i = 0; i < width_word; i++)
+                        {
+                            Color C = b7.GetPixel(i, j);
+                            if (C.R < 128) array_License_Plate[1, k, i, j] = 1;//黑點
+                        }
+                    }
+                }
+                //六碼變形的69字型
+                for(int k = 72; k < 74; k++)
+                {
+                    Bitmap b69 = new Bitmap(d + "\\" + k.ToString() + ".gif");
+                    for(int j = 0; j < height_word; j++)
+                    {
+                        for(int i = 0; i < width_word; i++)
+                        {
+                            Color C = b69.GetPixel(i, j);
+                            if (C.R < 128) array_Deform_69[k - 72, i, j] = 1;//黑點
+                        }
+                    }
+                }
+            }
+        }
+
+        //選擇字模
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int knd = listBox1.SelectedIndex;//六或七碼
+            if (knd < 0) return;//未選擇
+            int Sc = listBox2.SelectedIndex;//選擇的字元
+            if (Sc < 0) return;//未選擇
+            if (knd == 1 && Sc > 35) return;//七碼車牌字元無變形的69
+            array_Targect = new byte[width_word, height_word];
+            for(int i = 0; i < width_word; i++)
+            {
+                for(int j = 0; j < height_word; j++)
+                {
+                    if(knd==0)//六碼字型
+                    {
+                        if (Sc <= 35)
+                        {
+                            array_Targect[i, j] = array_License_Plate[0, Sc, i, j];
+                        }
+                        else
+                        {
+                            array_Targect[i, j] = array_Deform_69[Sc - 36, i, j];
+                        }
+                    }
+                    else//七碼字型
+                    {
+                        array_Targect[i, j] = array_License_Plate[1, Sc, i, j];
+                    }
+                }
+            }
+            pictureBox1.Image = f.BWImg(array_Targect);//顯示字模影像
+        }
+
+        //建立字模二進位檔案
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (System.IO.File.Exists(saveFileDialog1.FileName))
+                {
+                    System.IO.File.Delete(saveFileDialog1.FileName);
+                }
+            }
+            //寫出六與七碼字模
+            byte[] x = new byte[width_word * height_word * (36 * 2 + 2)];
+            int n = 0;
+            //六與七碼字模
+            for(int m = 0; m < 2; m++)
+            {
+                for(int k = 0; k < 36; k++)
+                {
+                    for(int j = 0; j < height_word; j++)
+                    {
+                        for(int i = 0; i < width_word; i++)
+                        {
+                            x[n] = array_License_Plate[m, k, i, j];
+                            n += 1;
+                        }
+                    }
+                }
+            }
+            //六碼變形69字模
+            for(int k = 0; k < 2; k++)
+            {
+                for(int j = 0; j < height_word; j++)
+                {
+                    for(int i = 0; i < width_word; i++)
+                    {
+                        x[n] = array_Deform_69[k, i, j];
+                        n += 1;
+                    }
+                }
+            }
+            //寫出檔案
+            System.IO.File.WriteAllBytes(saveFileDialog1.FileName, x);
+        }
+
+        //程式啟動載入字模
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FontLoad();
+        }
+        //載入字模
+        private void FontLoad()
+        {
+            byte[] q = Properties.Resources.font;
+            int n = 0;
+            for(int m = 0; m < 2; m++)
+            {
+                for(int k = 0; k < 36; k++)
+                {
+                    for (int j=0; j < height_word; j++)
+                    {
+                        for(int i = 0; i < width_word; i++)
+                        {
+                            array_License_Plate[m, k, i, j] = q[n];
+                            n += 1;
+                        }
+                    }   
+                }
+            }
+            for(int k = 0; k < 2; k++)
+            {
+                for(int j = 0; j < height_word; j++)
+                {
+                    for(int i = 0; i < width_word; i++)
+                    {
+                        array_Deform_69[k, i, j] = q[n];
+                        n += 1;
+                    }
+                }
+            }
+        }
+
+        //載入比對目標
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap m = new Bitmap(openFileDialog1.FileName);
+                pictureBox2.Image = m;
+                array_Compared_Font = new byte[width_word, height_word];//待比對的目標二值化陣列
+                for(int j = 0; j < height_word; j++)
+                {
+                    for(int i = 0; i < width_word; i++)
+                    {
+                        Color C = m.GetPixel(i, j);
+                        if (C.R < 127) array_Compared_Font[i, j] = 1;//黑點
+                    }
+                }
+            }
+        }
+
+        //執行字模比對
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n0 = 0;//字模黑點數
+            int nf = 0;//符合的黑點數
+            for(int j = 0; j < height_word; j++)
+            {
+                for(int i = 0; i < width_word; i++)
+                {
+                    if (array_Targect[i, j] == 0)
+                    {
+                        if (array_Compared_Font[i, j] == 1) nf -= 1;
+                    }
+                    else
+                    {
+                        n0 += 1;//字模黑點數累計
+                        if (array_Compared_Font[i, j] == 1) nf += 1;//目標與字模符合點數
+                    }
+                }
+            }
+            int pc = nf * 1000 / n0;//符合點數千分比
+            label1.Text = pc.ToString();
+        }
+    }
+}
